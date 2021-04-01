e314689ef80d5729be2339243b54a0925699de6c
geliang <528636582@qq.com>
2021-04-01 23:42:10 +0800

    no message

--- urlconfig.js
+++ urlconfig.js
@@ -1,5 +1,5 @@
 export default {
-    // BASE_URL: 'https://jinyiweikeji.com',
-    BASE_URL: 'https://jobhorse.cn',
+    BASE_URL: 'https://jinyiweikeji.com',
+   // BASE_URL: 'https://jobhorse.cn',
     AUTH_URL: 'xxxx'
 }
